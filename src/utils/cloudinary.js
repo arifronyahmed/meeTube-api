aadaf244d776384f2df9de136d86b989c610bf6f
Arif <arifronyahmed@gmail.com>
2024-01-23 14:39:06 +0100

    multer setup with cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,63 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+
+
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    //upload the fine on cloudinary
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    //file has been uploaded successfully
+
+    console.log("File is uploaded on cloudinary", response.url);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove temporary files 
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cloudinary.uploader.upload(
+  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function (error, result) {
+    console.log(result);
+  }
+);
